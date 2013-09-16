53d56a274e3afdc415eaba94ba073fb70a4edec2
Nikolay Martynov <nmartynov@shutterstock.com>
2013-09-16 11:36:53 -0400

    fix version

--- lib/Timed/Logger/Dancer/AdoptPlack.pm
+++ lib/Timed/Logger/Dancer/AdoptPlack.pm
@@ -14,11 +14,11 @@ Timed::Logger::Dancer::AdoptPlack - Use Plack Timed::Logger Middleware with Danc
 
 =head1 VERSION
 
-Version 0.01
+Version 0.0.1
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.0.1';
 
 =head1 SYNOPSIS
 
